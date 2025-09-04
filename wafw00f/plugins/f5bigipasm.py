9a25273e86c3249ae9f360256603fd79d8e3d087
HaimLC <110099998+HaimLC@users.noreply.github.com>
2025-09-04 15:36:51 +0300

    fix regex patterns for F5 BIG-IP ASM cookies

--- wafw00f/plugins/f5bigipasm.py
+++ wafw00f/plugins/f5bigipasm.py
@@ -11,12 +11,12 @@ def is_waf(self):
     if check_schema_01(self):
         return True
 
-    # ASM ≥ 11.4.0 → eight hex digits after “TS” - https://my.f5.com/manage/s/article/K6850
-    if self.matchCookie((r'^TS[a-fA-F0-9]{8}$', r'.+')):
+    # ASM ≥ 11.4.0 → eight hex digits after “TS” - https://my.f5.com/manage/s/article/K6850
+    if self.matchCookie(r'^TS[a-fA-F0-9]{8}=.+'):
         return True
     
-    # ASM 10.0.0 – 11.3.0 → six hex digits after “TS” - https://my.f5.com/manage/s/article/K6850
-    if self.matchCookie((r'^TS[a-fA-F0-9]{6}$', r'.+')):
+    # ASM 10.0.0 – 11.3.0 → six hex digits after “TS” - https://my.f5.com/manage/s/article/K6850
+    if self.matchCookie(r'^TS[a-fA-F0-9]{6}=.+'):
         return True
 
     return False
