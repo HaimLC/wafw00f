96268812089e44213fd4fe947b352595584c9804
HaimLC <110099998+HaimLC@users.noreply.github.com>
2025-09-04 15:27:42 +0300

    Fix matchCookie calls in f5bigipasm.py
    /usr/local/lib/python3.11/site-packages/wafw00f/main.py:285: in identwaf
        if self.wafdetections[wafvendor](self):
    /usr/local/lib/python3.11/site-packages/wafw00f/plugins/f5bigipasm.py:15: in is_waf
        if self.matchCookie((r'^TS[a-fA-F0-9]{8}$', r'.+')):
    /usr/local/lib/python3.11/site-packages/wafw00f/main.py:241: in matchCookie
        return self.matchHeader(('Set-Cookie', match), attack=attack)
    /usr/local/lib/python3.11/site-packages/wafw00f/main.py:225: in matchHeader
        if re.search(match, headerval, re.I):
    /usr/local/lib/python3.11/re/__init__.py:176: in search
        return _compile(pattern, flags).search(string)
    _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _ _
    pattern = ('^TS[a-fA-F0-9]{8}$', '.+'), flags = 2
        def _compile(pattern, flags):
            # internal: compile pattern
            if isinstance(flags, RegexFlag):
                flags = flags.value
            try:
                return _cache[type(pattern), pattern, flags]
            except KeyError:
                pass
            if isinstance(pattern, Pattern):
                if flags:
                    raise ValueError(
                        "cannot process flags argument with a compiled pattern")
                return pattern
            if not _compiler.isstring(pattern):
    >           raise TypeError("first argument must be string or compiled pattern")
    E           TypeError: first argument must be string or compiled pattern

--- wafw00f/plugins/f5bigipasm.py
+++ wafw00f/plugins/f5bigipasm.py
@@ -11,12 +11,14 @@ def is_waf(self):
     if check_schema_01(self):
         return True
 
-    # ASM ≥ 11.4.0 → eight hex digits after “TS” - https://my.f5.com/manage/s/article/K6850
-    if self.matchCookie((r'^TS[a-fA-F0-9]{8}$', r'.+')):
+    # ASM ≥ 11.4.0 → eight hex digits after “TS” - https://my.f5.com/manage/s/article/K6850
+    # The extra parentheses are removed from the line below
+    if self.matchCookie(r'^TS[a-fA-F0-9]{8}$', r'.+'):
         return True
     
-    # ASM 10.0.0 – 11.3.0 → six hex digits after “TS” - https://my.f5.com/manage/s/article/K6850
-    if self.matchCookie((r'^TS[a-fA-F0-9]{6}$', r'.+')):
+    # ASM 10.0.0 – 11.3.0 → six hex digits after “TS” - https://my.f5.com/manage/s/article/K6850
+    # The extra parentheses are also removed from the line below
+    if self.matchCookie(r'^TS[a-fA-F0-9]{6}$', r'.+'):
         return True
 
     return False
